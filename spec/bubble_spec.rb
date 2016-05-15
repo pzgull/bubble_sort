2763f64949013bb30a8cf93eaf1f4661c8d5df9a
Pierre Dimitrou <dimitriou.pierrea@gmail.com>
2016-05-15 21:05:38 +0200

    Replace match_array by match method to match the expected positions

--- spec/bubble_spec.rb
+++ spec/bubble_spec.rb
@@ -6,6 +6,6 @@ array = (1..10).to_a.shuffle
 RSpec.describe Bubble do
     it 'works' do
         bubble = Bubble.new(array)
-        expect(bubble.sort()).to match_array(array.sort)
+        expect(bubble.sort()).to match(array.sort)
     end
 end
